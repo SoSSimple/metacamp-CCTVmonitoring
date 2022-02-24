70bb8ebd8679ed1165cda03b3b392edc8ff4083c
kce <lock6241@naver.com>
2022-02-24 20:50:34 +0900

    chart.js 추가 & department store 추가

--- front-end/cctv-monitoring-frontend/src/views/dashboard/index.vue
+++ front-end/cctv-monitoring-frontend/src/views/dashboard/index.vue
@@ -7,20 +7,26 @@
     <b-container id="device-charts">
       <b-row>
         <b-col class="chart-cards">
-          <b-card title="설비 1">
-            <img src="../../assets/img/chart.png" alt="chart-img" />
+          <b-card :title="device[0].deviceName">
+            <div v-if="chartData">
+              <line-chart :chart-data="chartData" class="line-chart"></line-chart>
+            </div>
             <b-button block variant="primary">CCTV 보기</b-button></b-card
           >
         </b-col>
         <b-col class="chart-cards">
-          <b-card title="설비 2">
-            <img src="../../assets/img/chart.png" alt="chart-img" />
+          <b-card :title="device[1].deviceName">
+            <div v-if="chartData">
+              <line-chart :chart-data="chartData" class="line-chart"></line-chart>
+            </div>
             <b-button block variant="primary">CCTV 보기</b-button></b-card
           >
         </b-col>
         <b-col class="chart-cards">
-          <b-card title="설비 3">
-            <img src="../../assets/img/chart.png" alt="chart-img" />
+          <b-card :title="device[2].deviceName">
+            <div v-if="chartData" class="line-chart">
+              <line-chart :chart-data="chartData" class="line-chart"></line-chart>
+            </div>
             <b-button block variant="primary">CCTV 보기</b-button></b-card
           >
         </b-col>
@@ -30,7 +36,48 @@
 </template>
 
 <script>
-export default {}
+import LineChart from '@/components/chart/lineChart'
+export default {
+  components: {
+    'line-chart': LineChart
+  },
+  data() {
+    return {
+      chartData: null,
+      device: [
+        { deviceId: 1, deviceName: '설비 1', tagList: ['온도', '습도'] },
+        { deviceId: 2, deviceName: '설비 2', tagList: ['온도', '습도'] },
+        { deviceId: 3, deviceName: '설비 3', tagList: ['온도', '습도'] }
+      ]
+    }
+  },
+  mounted() {
+    this.makeChartData()
+  },
+  methods: {
+    makeChartData() {
+      this.chartData = {
+        labels: ['1sec', '2sec', '3sec', '4sec', '5sec'],
+        datasets: [
+          {
+            label: '온도',
+            data: [100, 120, 130, 150, 110],
+            fill: false,
+            borderColor: 'rgb(220,53,69)'
+            // tension: 0
+          },
+          {
+            label: '습도',
+            data: [111, 205, 150, 350, 200],
+            fill: false,
+            borderColor: 'rgb(0 123 255)'
+            // tension: 0
+          }
+        ]
+      }
+    }
+  }
+}
 </script>
 
 <style src="../../assets/style/dashboard.css"></style>
--- front-end/cctv-monitoring-frontend/src/views/department/index.vue
+++ front-end/cctv-monitoring-frontend/src/views/department/index.vue
@@ -10,16 +10,16 @@
         <b-col class="search-input">
           <b-input-group>
             <b-form-input placeholder="검색어를 입력해주세요."></b-form-input>
-            <b-button variant="primary">검색</b-button>
+            <b-button variant="primary" @click="searchDepartmentList">검색</b-button>
           </b-input-group>
         </b-col>
 
-        <b-button variant="primary">부서 등록</b-button>
+        <b-button variant="primary" @click="onClickAddNew">부서 등록</b-button>
       </b-row>
 
       <!-- table section -->
       <b-row class="table-section">
-        <b-table small head-variant="light" :items="items" :fields="fields">
+        <b-table small head-variant="light" :items="departmentList" :fields="fields">
           <!-- checkbox section -->
           <template #head(select)="row">
             <b-form-checkbox v-model="allSelected" @change="toggleAll">{{ row.id }}</b-form-checkbox>
@@ -28,21 +28,35 @@
             <b-form-checkbox v-model="selected" :value="row.item.id"></b-form-checkbox>
           </template>
 
+          <template #cell(createdAt)="row">
+            {{ row.item.createdAt.substring(0, 10) }}
+          </template>
+
           <!-- button section -->
           <template #cell(updateBtn)="row">
-            <b-button size="sm" variant="primary">수정 {{ row.id }}</b-button>
+            <b-button size="sm" variant="primary" :disabled="!(selected == '')" @click="onClickEdit(row.item.id)"
+              >수정</b-button
+            >
           </template>
           <template #cell(deleteBtn)="row">
-            <b-button size="sm" variant="danger">삭제 {{ row.id }}</b-button>
+            <b-button size="sm" variant="danger" @click="onClickDelete(row.item.id)">삭제</b-button>
           </template>
         </b-table>
       </b-row>
     </b-container>
+
+    <!-- modal section -->
+    <inform />
   </div>
 </template>
 
 <script>
+import inform from './inform.vue'
+
 export default {
+  components: {
+    inform: inform
+  },
   data() {
     return {
       fields: [
@@ -55,29 +69,161 @@ export default {
         { key: 'updateBtn', label: '수정' },
         { key: 'deleteBtn', label: '삭제' }
       ],
-      // fake data
-      items: [
-        { id: 1, name: 'department1', manager: '홍길동', member: 13 },
-        { id: 2, name: 'department2', manager: '김길동', member: 11 },
-        { id: 3, name: 'department3', manager: '최길동', member: 9 }
-      ],
       selected: [],
       allSelected: false
     }
   },
-  methods: {
-    toggleAll() {
-      // items는 fake data 이므로 이후 로직 작성시 주의
-      if (this.allSelected) {
-        this.selected = []
-        for (let i in this.items) {
-          this.selected.push(this.items[i].id)
-        }
-      } else {
-        for (let i in this.items) {
-          this.selected.pop(this.items[i].id)
+  computed: {
+    departmentList() {
+      return this.$store.getters.DepartmentList
+    },
+    insertedResult() {
+      return this.$store.getters.DepartmentInsertedResult
+    },
+    updatedResult() {
+      return this.$store.getters.DepartmentUpdatedResult
+    },
+    deletedResult() {
+      return this.$store.getters.DepartmentDeletedResult
+    }
+  },
+  watch: {
+    insertedResult(value) {
+      // 등록 후 메시지 출력 처리
+      if (value !== null) {
+        if (value > 0) {
+          // 등록이 성공한 경우
+
+          // 1. 메시지 출력
+          this.$bvToast.toast('부서 정보가 등록 되었습니다.', {
+            title: 'SUCCESS',
+            variant: 'success',
+            solid: true
+          })
+
+          // 2. 리스트 재검색
+          this.searchDepartmentList()
+        } else {
+          // 등록이 실패한 경우
+          this.$bvToast.toast('부서 정보 등록이 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
         }
       }
+    },
+    updatedResult(value) {
+      // 수정 후 메시지 출력 처리
+      if (value !== null) {
+        if (value > 0) {
+          // 수정이 성공한 경우
+
+          // 1. 메시지 출력
+          this.$bvToast.toast('부서 정보가 수정 되었습니다.', {
+            title: 'SUCCESS',
+            variant: 'success',
+            solid: true
+          })
+
+          // 2. 리스트 재검색
+          this.searchDepartmentList()
+        } else {
+          // 수정이 실패한 경우
+          this.$bvToast.toast('부서 정보 수정이 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
+        }
+      }
+    },
+    deletedResult(value) {
+      // 삭제 후 메시지 출력 처리
+      if (value !== null) {
+        if (value > 0) {
+          // 삭제가 성공한 경우
+
+          // 1. 메시지 출력
+          this.$bvToast.toast('부서 정보가 삭제 되었습니다.', {
+            title: 'SUCCESS',
+            variant: 'success',
+            solid: true
+          })
+
+          // 2. 리스트 재검색
+          this.searchDepartmentList()
+        } else {
+          // 삭제가 실패한 경우
+          this.$bvToast.toast('부서 정보 삭제가 실패하였습니다.', {
+            title: 'ERROR',
+            variant: 'danger',
+            solid: true
+          })
+        }
+      }
+    }
+  },
+  created() {
+    this.searchDepartmentList()
+  },
+  methods: {
+    // table checkbox method
+    toggleAll() {
+      if (this.allSelected) {
+        this.selected = []
+        for (let i in this.departmentList) {
+          this.selected.push(this.departmentList[i].id)
+        }
+      } else {
+        for (let i in this.departmentList) {
+          this.selected.pop(this.departmentList[i].id)
+        }
+      }
+    },
+
+    // department REST method
+    searchDepartmentList() {
+      this.$store.dispatch('actDepartmentList')
+    },
+    onClickAddNew() {
+      // 신규 등록
+
+      // 1. 입력모드 설정
+      this.$store.dispatch('actDepartmentInputMode', 'insert')
+
+      // 2. 상세정보 초기화
+      this.$store.dispatch('actDepartmentInit')
+
+      // 3. 모달 출력
+      this.$bvModal.show('modal-department-inform')
+    },
+    onClickEdit(id) {
+      // 수정 상세정보
+
+      // 1. 입력모드 설정
+      this.$store.dispatch('actDepartmentInputMode', 'update')
+
+      // 2. 상세정보 호출
+      this.$store.dispatch('actDepartmentInfo', id)
+
+      // 3. 모달 출력
+      this.$bvModal.show('modal-department-inform')
+    },
+    onClickDelete(id) {
+      // 삭제
+      this.$bvModal.msgBoxConfirm('삭제하시겠습니까?').then(value => {
+        if (value && this.selected) {
+          // console.log('onClickDelete')
+          for (let i = 0; i < this.selected.length; i += 1) {
+            this.$store.dispatch('actDepartmentDelete', this.selected[i])
+          }
+        }
+        if (value && this.selected == '') {
+          console.log('onClickDelete-noSelect')
+          this.$store.dispatch('actDepartmentDelete', id)
+        }
+      })
     }
   }
 }
